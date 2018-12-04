188db656bdc3f6d36cf23123a602cb83bc61d8d3
Arman Sujoyan <arman@brainbox.am>
2018-12-04 16:13:14 +0400

    Add CRUD implementation

--- users/tests/create_test.js
+++ users/tests/create_test.js
@@ -2,12 +2,13 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     const joe = new User({ name: 'Joe' });
 
     joe.save()
       .then((joe) => {
-        
+        assert(!joe.isNew);
+        done();
       });
   });
 });
--- users/tests/test_helper.js
+++ users/tests/test_helper.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
-mongoose.connection
-  .once('open', () => console.log('Ready to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+
+before((done) => {
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
