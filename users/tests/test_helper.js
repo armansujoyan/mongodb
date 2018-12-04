fb9b52b07d163ca7ba21213e601a5ed536b1d145
Arman Sujoyan <arman@brainbox.am>
2018-12-04 14:35:29 +0400

    Adding users

---
+++ users/tests/test_helper.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
+mongoose.connection
+  .once('open', () => console.log('Ready to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    done();
+  });
+});
