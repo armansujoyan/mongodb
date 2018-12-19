a491648dff800005c2ed0972217b7efa962f572a
Arman Sujoyan <asujoyan@gmail.com>
2018-12-19 19:05:19 +0400

    Add test for Operator

--- users/tests/update_test.js
+++ users/tests/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -39,5 +39,14 @@ describe('Updating records', () => {
 
   it('A model class can update a record based on ID', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user post count increment', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
