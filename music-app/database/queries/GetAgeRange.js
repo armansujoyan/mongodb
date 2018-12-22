2ec51506dd4db84235e4e3af5a94ae0ca967f573
Arman Sujoyan <asujoyan@gmail.com>
2018-12-23 01:26:34 +0400

    Add getRange query.

--- music-app/database/queries/GetAgeRange.js
+++ music-app/database/queries/GetAgeRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    return new Promise((resolve, reject) => {
+        let result = { min: -Infinity, max: Infinity };
+
+        const minQuery = Artist.find({})
+            .sort('age')
+            .limit(1);
+
+        const maxQuery = Artist.find({})
+            .sort({ age: -1 })
+            .limit(1);
+
+        Promise.all([minQuery, maxQuery])
+            .then((queries) => {
+                result.min = queries[0][0].age;
+                result.max = queries[1][0].age;
+            resolve(result);
+            });
+    });
 };
